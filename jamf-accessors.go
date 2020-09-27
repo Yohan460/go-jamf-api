c6c33512b852aa8ca57eef802e132c499a8879ad
sioncojp <sion_cojp@yahoo.co.jp>
2020-09-28 08:59:34 +0900

    add: accessors

--- jamf-accessors.go
+++ jamf-accessors.go
@@ -7,3 +7,17 @@ func (d *Department) SetName(v string) {
 func (d *Department) SetId(v string) {
 	d.Id = &v
 }
+
+func (d *Department) GetName() string {
+	if d == nil || d.Name == nil {
+		return ""
+	}
+	return *d.Name
+}
+
+func (d *Department) GetId() string {
+	if d == nil || d.Id == nil {
+		return ""
+	}
+	return *d.Id
+}
