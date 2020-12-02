c510dc56539005c6136a92b47984c36a9a310295
Johan McGwire <johan.mcgwire@coinbase.com>
2020-12-02 11:34:26 -0500

    int -> script

--- scripts.go
+++ scripts.go
@@ -94,7 +94,7 @@ func (c *Client) UpdateScript(d *Script) (*Script, error) {
 	return script, err
 }
 
-func (c *Client) DeleteScript(id int) (string, error) {
+func (c *Client) DeleteScript(id string) (string, error) {
 
 	group := &Script{}
 	err := c.doRequest("DELETE", fmt.Sprintf("%s/id/%v", uriScripts, id), nil, group)
