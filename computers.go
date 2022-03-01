6df98ad330bd1f150aafe92eab32227b992bbf20
Johan McGwire <johan@mcgwire.tech>
2022-03-01 14:49:00 -0500

    Add UDID

--- computers.go
+++ computers.go
@@ -11,6 +11,7 @@ type ComputersResponse struct {
 
 type ComputerListResponse struct {
 	ID              int    `json:"id,omitempty" xml:"id,omitempty"`
+	UDID            string `json:"udid,omitempty" xml:"udid,omitempty"`
 	Name            string `json:"name,omitempty" xml:"name,omitempty"`
 	SerialNumber    string `json:"serial_number,omitempty" xml:"serial_number,omitempty"`
 	Managed         bool   `json:"managed,omitempty" xml:"managed,omitempty"`
